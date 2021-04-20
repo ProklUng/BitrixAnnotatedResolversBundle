475bbf5693ec43d7d7d77898d7addeb8c95c6f36
Fedor <funtik21975@gmail.com>
2021-04-20 19:14:58 +0300

    fixes

---
+++ Exceptions/BitrixFileNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Prokl\BitrixAnnotatedResolversBundle\Exceptions;
+
+use Symfony\Component\HttpFoundation\Exception\RequestExceptionInterface;
+
+/**
+ * Class BitrixFileNotFoundException
+ * @package Prokl\BitrixAnnotatedResolversBundle\Exceptions
+ *
+ * @sinсe 02.04.2021
+ */
+class BitrixFileNotFoundException extends BaseException implements RequestExceptionInterface
+{
+
+}
